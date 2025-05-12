68b0465586ac33aa73e4ca369c55b626bcffd3f8
Eugene B. <beugenecarlo@gmail.com>
2025-05-12 11:08:13 +0200

    Updated: import langchain.chatmodels to langchain_community.models. Reason: langchain.mopdels depreciated.

--- app/groq_utils.py
+++ app/groq_utils.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
+from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
 from langchain.prompts import PromptTemplate
 from langchain.chains import ConversationChain
